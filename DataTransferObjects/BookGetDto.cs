143ad1763d39636fcdcbea1d52c0f681c02510be
Iza <mrs.master.of.disaster@gmail.com>
2025-06-04 14:36:45 +0200

    Dodano filtry (działające). Dodano wyszukiwanie - na razie działa tylko na stronie wyników.

--- DataTransferObjects/BookGetDto.cs
+++ DataTransferObjects/BookGetDto.cs
@@ -13,12 +13,23 @@ namespace BibliotekaSzkolnaBlazor.DataTransferObjects
         public bool IsDeleted { get; set; }
         public bool IsVisible { get; set; }
         public string? ImageUrl { get; set; }
+        public int BookAuthorId { get; set; }
         public string BookAuthor { get; set; } = null!;
+
+        public int BookPublisherId { get; set; }
         public string BookPublisher { get; set; } = null!;
-        public string BookSeries { get; set; } = null!;
+
+        public int? BookSeriesId { get; set; }
+        public string? BookSeries { get; set; }
+
+        public int BookCategoryId { get; set; }
         public string BookCategory { get; set; } = null!;
+
+        public int BookTypeId { get; set; }
         public string BookType { get; set; } = null!;
-        public List<string> BookGenres { get; set; } = null!;
+
+        public List<int> BookGenreIds { get; set; } = new();
+        public List<string> BookGenres { get; set; } = new();
         public List<string>? BookSpecialTags { get; set; }
         public List<CopyGetDto>? BookCopies { get; set; }
         public int CopyCount;
