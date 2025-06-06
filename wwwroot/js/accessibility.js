d550ae2d9ffacad02644bb396ac8c8343d6fe0cb
Iza <mrs.master.of.disaster@gmail.com>
2025-06-06 10:38:29 +0200

    Dodano widocznosc panelu admina tylko dla wybranych ról. Zmieniono stronę główną na katalog.

---
+++ wwwroot/js/accessibility.js
@@ -0,0 +1,28 @@
+﻿//zmiana motywu, kontrastu, wielkosci czcionki
+window.applyUserPreferences = () => {
+    const body = document.body;
+    if (!body) return;
+
+    const savedTheme = localStorage.getItem('theme');
+    const savedFontSize = localStorage.getItem('fontSize');
+
+    body.classList.remove(...Array.from(body.classList).filter(cls => cls.startsWith('theme-')));
+    if (savedTheme) {
+        body.classList.add(savedTheme);
+    }
+
+    body.classList.remove(...Array.from(body.classList).filter(cls => cls.startsWith('font-')));
+    if (savedFontSize) {
+        body.classList.add(savedFontSize);
+    }
+};
+
+window.setTheme = (themeClass) => {
+    localStorage.setItem('theme', themeClass);
+    window.applyUserPreferences();
+};
+
+window.setFontSize = (fontSizeClass) => {
+    localStorage.setItem('fontSize', fontSizeClass);
+    window.applyUserPreferences();
+};
